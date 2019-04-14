a1d455f38950d719c603f9d06c2cc86fe72bf3d0
Victor Dominoni <dominoni.victor@gmail.com>
2019-04-14 11:35:56 -0300

    Versão 0.1 - Funcionalidade básica

--- whatsapp.py
+++ whatsapp.py
@@ -1,20 +1,28 @@
 #Send automated whatsapp messages
 from selenium import webdriver
 
+#Abre o chrome e salva a uma variavel driver
 driverPath = "C:\\Users\\tamir\\Documents\\Victor\\Subfiles_for_python_stuff\\chrome_driver\\chromedriver.exe"
 driver = webdriver.Chrome(driverPath)
+
+#Abre a pagina do whatsapp
 driver.get('https://web.whatsapp.com/')
+
+#Recebe o nome do usuário/grupo para quem as mensagens serão mandadas assim como a mensagem a ser mandada
 name =  'Tarefas'
 msg = 'Parabens você foi hackeado!'
 
+#Espera o usuário fazer o scan do código QR no browser para executar o resto do código
 input('Pressione qualquer tecla após o scan')
-#ola eu sou um comentario
+
+#Encontra o usuário colocado em name e clica nele
 user = driver.find_element_by_xpath('//span[@title="{}"]'.format(name))
 user.click()
 
-for i in range(10): #0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-    msg_box = driver.find_element_by_xpath('//div[@class = "_2S1VP copyable-text selectable-text"]')
-    msg_box.send_keys(msg)
-    button = driver.find_element_by_xpath('//button[@class = "_35EW6"]')
-    button.click()
-   
+#Encontra a caixa de mensagem e escreve a msg nela
+msg_box = driver.find_element_by_xpath('//div[@class = "_2S1VP copyable-text selectable-text"]')
+msg_box.send_keys(msg)
+
+#Encontra o botão de enviar e clica nele
+button = driver.find_element_by_xpath('//button[@class = "_35EW6"]')
+button.click()
