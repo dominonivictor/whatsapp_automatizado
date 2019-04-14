e5eb9b43e2a1bd028286060342432bb2f48cba1a
Victor Dominoni <dominoni.victor@gmail.com>
2019-04-14 15:52:43 -0300

    Versão 0.4 - Modularização do código, me preparando para objetos!

--- webzap.py
+++ webzap.py
@@ -2,8 +2,35 @@
 from selenium import webdriver
 from time import sleep
 
-#função para mandar 1 mensagem para diversos usuários 1 vez, com opção de aumentar o numero de mensagens
+def fazer_decisao(names, msg, vezes=1):
+    decisao = ''
+    #Recebe o nome dos usuários/grupos para quem as mensagens serão mandadas assim como a mensagem a ser mandada
+    print('Deseja alterar os contatos? (s/n) \nContatos atualmente: {}'.format(', '.join(names)))
+    decisao = input().lower()
+    if decisao == 's':
+        names = input('''Insira os nomes dos contatos EXATAMENTE como estão no seu Whatsapp
+                         separados APENAS por virgula. ex: Joao Pedro,Maria,Jose \n''').split(',')
+    decisao = ''   
+    #Recebe o nome dos usuários/grupos para quem as mensagens serão mandadas assim como a mensagem a ser mandada
+    print('Deseja alterar a mensagem? (s/n) \nMensagem atual: {}'.format(msg))
+    decisao = input().lower()    
+    if decisao == 's':
+        msg = input('Insira a mensagem que deseja mandar para os contatos escolhidos \n')
+    decisao = '' 
+    #Recebe o numero de vezes que o usuário deseja mandar a mensagem        
+    print('Deseja alterar o numero de vezes? (s/n) \nNúmero de vezes atual: {}'.format(vezes))
+    decisao = input().lower()
+    if decisao == 's':
+        vezes = int(input('Insira o numero de vezes que deseja mandar a mensagem: \n'))
+    decisao = ''
+    
+    
+    return (names, msg, vezes)
+
+#função para mandar 1 mensagem para diversos usuários 1 vez, com opção de aumentar o numero de mensagens, retorna os novos valores de contatos, msgs e vezes
 def mandar_msg(driver, names, msg, vezes=1):
+
+    names, msg, vezes = fazer_decisao(names, msg, vezes)
     #Aqui itera pela lista de nomes chamando cada item da iteração de nome
     for name in names:
         #Encontra o usuário colocado em name e clica nele
@@ -19,6 +46,18 @@ def mandar_msg(driver, names, msg, vezes=1):
             button = driver.find_element_by_xpath('//button[@class = "_35EW6"]')
             button.click()
             
-        #Espera 0.5 segundos antes de passar para a próxima etapa, pois ocorreu um bug ao tentar clicar em uma mensagem
+        #Espera 0.1 segundos antes de passar para a próxima etapa, pois ocorreu um bug ao tentar clicar em uma mensagem
         #para outros usuarios quase ao mesmo tempo que estavam digitando e enviando a mensagem
-        sleep(0.5)
+        sleep(0.1)
+    
+    return names, msg, vezes
+        
+def abre_driver():
+    #Abre o chrome e salva a uma variavel driver
+    driverPath = "C:\\Users\\tamir\\Documents\\Victor\\Subfiles_for_python_stuff\\chrome_driver\\chromedriver.exe"
+    driver = webdriver.Chrome(driverPath)
+
+    #Abre a pagina do whatsapp
+    driver.get('https://web.whatsapp.com/')
+    return driver
+    
--- whatsapp.py
+++ whatsapp.py
@@ -1,59 +1,29 @@
 #Send automated whatsapp messages
 from selenium import webdriver
-from webzap import mandar_msg
+from webzap import mandar_msg, abre_driver
 
-
-#Abre o chrome e salva a uma variavel driver
-driverPath = "C:\\Users\\tamir\\Documents\\Victor\\Subfiles_for_python_stuff\\chrome_driver\\chromedriver.exe"
-driver = webdriver.Chrome(driverPath)
-
-#Abre a pagina do whatsapp
-driver.get('https://web.whatsapp.com/')
+#abre uma nova janela no site do Web WhatsApp
+driver = abre_driver()
 
 #Variaveis pré definidas
 names =  ['Tarefas', 'deutsch', 'Super Teste']
 msg = 'Isso é uma tentativa de mandar 1 mensagens para multiplos grupos/usuários'
 decisao = ''
-vezes = 1
+vezes=1
+repetir = True
 
 #Espera o usuário fazer o scan do código QR no browser para executar o resto do código
 input('Pressione qualquer tecla após o scan')
 
 
-
-#Recebe o nome dos usuários/grupos para quem as mensagens serão mandadas assim como a mensagem a ser mandada
-print('Deseja alterar os contatos? (s/n) \nContatos atualmente: {}'.format(', '.join(names)))
-decisao = input().lower()
-
-if decisao == 's':
-
-    names = input('''Insira os nomes dos contatos EXATAMENTE como estão no seu Whatsapp \n
-                separados APENAS por virgula. ex: Joao Pedro,Maria,Jose''').split(',')
-    decisao = ''
-elif decisao == 'n':
-    decisao = ''
+while repetir == True:
     
-
-#Recebe o nome dos usuários/grupos para quem as mensagens serão mandadas assim como a mensagem a ser mandada
-print('Deseja alterar a mensagem? (s/n) \nMensagem atual: {}'.format(msg))
-decisao = input().lower()
+    names, msg, vezes = mandar_msg(driver, names, msg, vezes=vezes)
     
-if decisao == 's':
-    msg = input('Insira a mensagem que deseja mandar para os contatos escolhidos \n')
-    decisao = ''
-elif decisao == 'n':
-    decisao = ''
+    print('Deseja repetir toda a operação?(s/n)') 
     
-print('Deseja alterar o numero de vezes? (s/n) \nNúmero de vezes atual: {}'.format(vezes))
-decisao = input().lower()
+    if input().lower() == 'n': repetir = False
     
-if decisao == 's':
-    vezes = input('Insira o numero de vezes que deseja mandar a mensagem: \n')
-    decisao = ''
-elif decisao == 'n':
-    decisao = ''    
-    
-mandar_msg(driver, names, msg, vezes=vezes)
 
 
     
