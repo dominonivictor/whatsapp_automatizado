24d03d021130189c8913eec01d476e9779015e4e
Victor Dominoni <dominoni.victor@gmail.com>
2019-04-14 12:15:03 -0300

    Versão 0.11 - Mandar mensagens para multiplos usuários

--- whatsapp.py
+++ whatsapp.py
@@ -1,5 +1,6 @@
 #Send automated whatsapp messages
 from selenium import webdriver
+from time import sleep
 
 #Abre o chrome e salva a uma variavel driver
 driverPath = "C:\\Users\\tamir\\Documents\\Victor\\Subfiles_for_python_stuff\\chrome_driver\\chromedriver.exe"
@@ -8,21 +9,27 @@ driver = webdriver.Chrome(driverPath)
 #Abre a pagina do whatsapp
 driver.get('https://web.whatsapp.com/')
 
-#Recebe o nome do usuário/grupo para quem as mensagens serão mandadas assim como a mensagem a ser mandada
-name =  'Tarefas'
-msg = 'Parabens você foi hackeado!'
+#Recebe o nome dos usuários/grupos para quem as mensagens serão mandadas assim como a mensagem a ser mandada
+names =  ['Tarefas', 'deutsch', 'Super Teste', 'Mini Sch@tli SP']
+msg = 'Isso é uma tentativa de mandar 1 mensagens para multiplos grupos/usuários'
 
 #Espera o usuário fazer o scan do código QR no browser para executar o resto do código
 input('Pressione qualquer tecla após o scan')
 
-#Encontra o usuário colocado em name e clica nele
-user = driver.find_element_by_xpath('//span[@title="{}"]'.format(name))
-user.click()
+#Aqui itera pela lista de nomes chamando cada item da iteração de nome
+for name in names:
+    #Encontra o usuário colocado em name e clica nele
+    user = driver.find_element_by_xpath('//span[@title="{}"]'.format(name))
+    user.click()
 
-#Encontra a caixa de mensagem e escreve a msg nela
-msg_box = driver.find_element_by_xpath('//div[@class = "_2S1VP copyable-text selectable-text"]')
-msg_box.send_keys(msg)
+    #Encontra a caixa de mensagem e escreve a msg nela
+    msg_box = driver.find_element_by_xpath('//div[@class = "_2S1VP copyable-text selectable-text"]')
+    msg_box.send_keys(msg)
 
-#Encontra o botão de enviar e clica nele
-button = driver.find_element_by_xpath('//button[@class = "_35EW6"]')
-button.click()
+    #Encontra o botão de enviar e clica nele
+    button = driver.find_element_by_xpath('//button[@class = "_35EW6"]')
+    button.click()
+    
+    #Espera 0.5 segundos antes de passar para a próxima etapa, pois ocorreu um bug ao tentar clicar em uma mensagem
+    #para outros usuarios quase ao mesmo tempo que estavam digitando e enviando a mensagem
+    sleep(0.5)
